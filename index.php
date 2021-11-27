4166a3ae9687ede4bf436a699e412d7483bcfffe
adawey <ahmed262715@arts.zu.edu.eg>
2021-11-28 01:08:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,700,900&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="contact/fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="contact/css/owl.carousel.min.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="contact/css/bootstrap.min.css">
+
+  <!-- Style -->
+  <link rel="stylesheet" href="contact/css/style.css">
+
+  <title>Contact Form #3</title>
+</head>
+
+<body>
+
+
+  <div class="content">
+
+    <div class="container">
+      <div class="row align-items-center">
+        jjj
+
+        <div class="col-lg-6">
+          <div class="box">
+            <h3 class="heading">Send us a message</h3>
+            <form class="mb-5" method="post" id="contactForm" name="contactForm">
+              <div class="row">
+
+                <div class="col-md-12 form-group">
+                  <label for="name" class="col-form-label">Name</label>
+                  <input type="text" class="form-control" name="name" id="name">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label for="email" class="col-form-label">Email</label>
+                  <input type="text" class="form-control" name="email" id="email">
+                </div>
+              </div>
+
+              <div class="row mb-3">
+                <div class="col-md-12 form-group">
+                  <label for="message" class="col-form-label">Message</label>
+                  <textarea class="form-control" name="message" id="message" cols="30" rows="7"></textarea>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <input type="submit" value="Send Message" class="btn btn-block btn-primary rounded-0 py-2 px-4">
+                  <span class="submitting"></span>
+                </div>
+              </div>
+            </form>
+
+            <div id="form-message-warning mt-4"></div>
+            <div id="form-message-success">
+              Your message was sent, thank you!
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <script src="contact/js/jquery-3.3.1.min.js"></script>
+    <script src="contact/js/popper.min.js"></script>
+    <script src="contact/js/bootstrap.min.js"></script>
+    <script src="contact/js/jquery.validate.min.js"></script>
+    <script src="contact/js/main.js"></script>
+</body>
+
+</html>
